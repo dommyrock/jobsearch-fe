e6df4ef52f127d2e5ba045b51c838c38cb2701e6
dominik-polzer <dominik.polzer-fve@bullhorn.com>
2022-05-21 23:23:40 +0200

    added socials container, .ico

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import SocialsContainer from '../components/common/SocialsContainer'
 
 const Home: NextPage = () => {
   return (
@@ -20,16 +21,8 @@ const Home: NextPage = () => {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <h4 className='text-sm text-slate-500'>Socials:</h4>
+        <SocialsContainer />
       </footer>
     </div>
   )
